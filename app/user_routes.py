f92bae95278640bab729aac76d8d0749cbdf490a
24f100ds2698 <24f1002698@ds.study.iitm.ac.in>
2025-03-30 00:45:17 +0530

    Update user_routes.py

--- app/user_routes.py
+++ app/user_routes.py
@@ -1,1 +1,9 @@
+from flask import Blueprint, rende_template
+from flask_login import login_required
 
+user=Blueprint('user',__name__)
+
+@user.route('/dashborad')
+@login_required
+def dashboard():
+  return render_template('user_dashboard.html')

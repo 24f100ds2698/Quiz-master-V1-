2358b6023b9374bc904baa066a949dccc7121995
24f100ds2698 <24f1002698@ds.study.iitm.ac.in>
2025-03-30 00:54:34 +0530

    Update quiz_routes.py

--- app/quiz_routes.py
+++ app/quiz_routes.py
@@ -1,1 +1,9 @@
+from flask import Blueprint,render_template
+
+quiz=Blueprint('quiz',__name__)
+
+@quiz.route('/attempt/<quiz_id>')
+def attempt_quiz(quiz_id):
+  return render_template('quiz.html',quiz_id=quiz_id)
+  
 

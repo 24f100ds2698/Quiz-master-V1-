184ac37e08e3514d2524dc9435305a4b3e8b9c59
24f100ds2698 <24f1002698@ds.study.iitm.ac.in>
2025-03-30 01:02:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+import os
 
+class Config:
+  SECRET_KEY=os.environ.get('SECRET_KEY') or 'your_secret_key'
+  SQLALCHEMY_DATABASE_URI='sqlite:///quizmaster.db'
+  SQLALCHEMY_TRACK_MODIFICATIONS= False 

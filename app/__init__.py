25690a67e95c30322034297b52088a07f2b816b8
24f100ds2698 <24f1002698@ds.study.iitm.ac.in>
2025-03-30 18:05:32 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,32 +7,27 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizmaster.db'
+    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///quizmaster.db"
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    
-    from .models import User  
+    from .models import User 
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .routes import main
-    from .admin_routes import admin
-    from .user_routes import user
-    from .quiz_routes import quiz
+    from .routes import main  
+    from .admin_routes import admin  
+    from .user_routes import user  
+    from .quiz_routes import quiz  
 
+  
     app.register_blueprint(main)
     app.register_blueprint(admin, url_prefix='/admin')
     app.register_blueprint(user, url_prefix='/user')
     app.register_blueprint(quiz, url_prefix='/quiz')
 
     return app
-
-
-
-
-
